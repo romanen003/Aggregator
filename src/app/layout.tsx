270851e4722fcd6c1b651391b1173585936942bd
Roman <romanen003@gmail.com>
2023-12-12 18:03:13 +0200

    add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from 'react';
 import type { Metadata } from 'next';
 // eslint-disable-next-line camelcase,import/order
 import { Source_Sans_3 } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import './styles/globals.css';
 import clsx from 'clsx';
@@ -27,23 +28,24 @@ export default function RootLayout({
 }: RootLayoutPropTypes) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-    <div className={styles.Layout}>
-      <div className={styles.Layout__header}>
-        <div className={styles.Layout__center}>
-          <Header />
+      <body className={inter.className}>
+        <div className={styles.Layout}>
+          <div className={styles.Layout__header}>
+            <div className={styles.Layout__center}>
+              <Header />
+            </div>
+          </div>
+          <div className={clsx(styles.Layout__center, styles.Layout__main)}>
+            {children}
+          </div>
+          <div className={styles.Layout__footer}>
+            <div className={styles.Layout__center}>
+              <Footer />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className={clsx(styles.Layout__center, styles.Layout__main)}>
-        {children}
-      </div>
-      <div className={styles.Layout__footer}>
-        <div className={styles.Layout__center}>
-          <Footer />
-        </div>
-      </div>
-    </div>
-    </body>
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
